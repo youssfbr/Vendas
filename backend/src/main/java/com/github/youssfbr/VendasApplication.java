255ad9b2427d54506b8075077a8f400a76d058f8
Alisson Youssf <youssfbr@gmail.com>
2021-03-04 14:18:06 -0300

    Spring Boot Test

--- backend/src/main/java/com/github/youssfbr/VendasApplication.java
+++ backend/src/main/java/com/github/youssfbr/VendasApplication.java
@@ -2,9 +2,18 @@ package com.github.youssfbr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
