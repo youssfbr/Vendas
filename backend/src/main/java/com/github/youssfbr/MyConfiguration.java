66ec0b8659752af266b25d2c97196b70bbdfaf3f
Alisson Youssf <youssfbr@gmail.com>
2021-03-04 14:41:46 -0300

    MyConfiguration Test

---
+++ backend/src/main/java/com/github/youssfbr/MyConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName() {
+        return "Sistema de Vendas";
+    }
+}
